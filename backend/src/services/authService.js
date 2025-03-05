db7ae6f977d7f26bfc072e5ab7ac13859214c8db
Janos Stelli <stellijanos23@gmail.com>
2025-03-05 13:28:45 +0200

    feat: add login method in auth service

--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -6,7 +6,18 @@ const userRepository = require('../repositories/userRepository');
 
 const ACCESS_TOKEN_EXPIRES_IN = env.auth.jwt.expiresIn.accessToken;
 const REFRESH_TOKEN_EXPIRES_IN = env.auth.jwt.expiresIn.refreshToken;
-const RESET_PASSWORD_TOKEN_EXPIRES_IN = env.auth.jwt.expiresIn.resetPasswordToken;
+const RESET_PASSWORD_TOKEN_EXPIRES_IN =
+    env.auth.jwt.expiresIn.resetPasswordToken;
+
+const getTokens = (payload) => {
+    return {
+        accessToken: jwtUtil.generateToken(jwtPayload, ACCESS_TOKEN_EXPIRES_IN),
+        refreshToken: jwtUtil.generateToken(
+            jwtPayload,
+            REFRESH_TOKEN_EXPIRES_IN
+        ),
+    };
+};
 
 const register = async (data) => {
     const { firstName, lastName, email, phone, birthDate, password, gender } =
@@ -37,21 +48,31 @@ const register = async (data) => {
         role: savedUser.role,
     };
 
-    const accessToken = jwtUtil.generateToken(
-        jwtPayload,
-        ACCESS_TOKEN_EXPIRES_IN
+    return getTokens();
+};
+
+const login = async ({ credential, password }) => {
+    const existingUser = await userRepository.getByEmailOrPhone(
+        credential,
+        credential
     );
-    const refreshToken = jwtUtil.generateToken(
-        jwtPayload,
-        REFRESH_TOKEN_EXPIRES_IN
+    if (!existingUser) {
+        throw new ErrorResponse(401, 'Invalid email or phone.');
+    }
+
+    const correctPassword = await bcryptUtil.comparePasswords(
+        password,
+        existingUser.password
     );
 
-    return {
-        accessToken,
-        refreshToken,
-    };
+    if (!correctPassword) {
+        throw new ErrorResponse(401, 'Incorrect password.');
+    }
+
+    return getTokens();
 };
 
 module.exports = {
     register,
+    login,
 };
