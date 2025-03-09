4e6bf0ee9351a31247ab073e0d81e992f50601e1
Janos Stelli <stellijanos23@gmail.com>
2025-03-09 13:33:54 +0200

    feat: add measurements retrieval by user

--- backend/src/routes/measurementRouter.js
+++ backend/src/routes/measurementRouter.js
@@ -4,5 +4,6 @@ const measurementController = require('../controllers/measurementController');
 const router = express.Router();
 
 router.post('/', measurementController.create);
+router.get('/', measurementController.getAllByUserId);
 
 module.exports = router;
