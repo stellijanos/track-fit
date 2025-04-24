dadddffc207e7b32718f5ec955f47978512527ed
Janos Stelli <stellijanos23@gmail.com>
2025-04-24 17:54:04 +0300

    feat: caloric target state

---
+++ frontend/src/app/core/states/caloric-target.state.ts
@@ -0,0 +1,50 @@
+import { Injectable, signal, computed } from '@angular/core';
+import { CaloricTarget, CaloricTargetRequest } from '../models/caloric-target.model';
+import { CaloricTargetApiService } from '../services/caloric-target-api.service';
+
+@Injectable({ providedIn: 'root' })
+export class CaloricTargetState {
+    private _targets = signal<CaloricTarget[]>([]);
+    readonly targets = computed(() => this._targets());
+
+    constructor(private api: CaloricTargetApiService) { }
+
+    clearTargets() {
+        this._targets.set([]);
+    }
+
+    loadTargets(force = false) {
+        if (force || this._targets().length === 0) {
+            this.api.getAll().subscribe({
+                next: (res) => {
+                    this._targets.set(res.data.caloricTargets);
+                },
+                error: (err) => {
+                    console.error(err.error.message);
+                }
+            });
+        }
+    }
+
+    createTarget(data: CaloricTargetRequest) {
+        this.api.create(data).subscribe({
+            next: (res) => {
+                this._targets.update(list => [...list, res.data.caloricTarget]);
+            },
+            error: (err) => {
+                console.error(err.error.message);
+            }
+        });
+    }
+
+    deleteTarget(id: string) {
+        this.api.delete(id).subscribe({
+            next: () => {
+                this._targets.update(list => list.filter(t => t.id !== id));
+            },
+            error: (err) => {
+                console.error(err.error.message);
+            }
+        });
+    }
+}
