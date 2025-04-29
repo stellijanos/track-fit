a378bb57d0d4005627c756275d950845ddb473cf
Janos Stelli <stellijanos23@gmail.com>
2025-04-29 19:25:49 +0300

    fix: user update validator name

--- backend/src/validators/user.js
+++ backend/src/validators/user.js
@@ -1,14 +1,15 @@
 const Joi = require('joi');
 const genders = require('../enums/genders');
 
-const updateById = Joi.object({
+const updateMe = Joi.object({
     firstName: Joi.string(),
     lastName: Joi.string(),
     email: Joi.string().email(),
     phone: Joi.string()
         .pattern(/^\+40\d{9}$/)
         .messages({
-            'string.pattern.base': 'Phone number must start with +40 and have exactly 9 digits after.',
+            'string.pattern.base':
+                'Phone number must start with +40 and have exactly 9 digits after.',
         }),
     birthDate: Joi.date(),
     gender: Joi.string().valid(...Object.values(genders)),
@@ -16,5 +17,5 @@ const updateById = Joi.object({
 }).min(1);
 
 module.exports = {
-    updateById,
+    updateMe,
 };
